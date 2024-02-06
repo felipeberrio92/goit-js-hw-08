a2026a092fb24cb6c72cf84fdce84b7df9126a75
felipeberrio92 <felipeberriomarin92@gmail.com>
2024-02-05 22:17:42 -0500

    tarea 8 finalizada

---
+++ src/js/03-feedback.js
@@ -0,0 +1,51 @@
+
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveFormState = () => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+};
+
+const loadFormState = () => {
+  const savedFormState = localStorage.getItem('feedback-form-state');
+
+  if (savedFormState) {
+    const parsedFormState = JSON.parse(savedFormState);
+    emailInput.value = parsedFormState.email;
+    messageInput.value = parsedFormState.message;
+  }
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formState);
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+const handleInput = throttle(() => {
+  saveFormState();
+}, 500);
+
+emailInput.addEventListener('input', handleInput);
+messageInput.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+
+loadFormState();
+const hello = 1;
